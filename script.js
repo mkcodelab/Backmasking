6d30fcc9eaeb0da4627bb923ce4972f2cb0f9897
mikcodelab <michal.kulawik@gmail.com>
2021-08-23 14:45:57 +0200

    deleted unused code

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const input = document.getElementById('fileInput');
-// const audio1 = document.getElementById('audio1');
 const processBtn = document.getElementById('process');
+
 let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 let audioEl;
-
-
 
 processBtn.addEventListener('click', process);
 
@@ -19,9 +17,6 @@ function process() {
     audioCtx.decodeAudioData(ev.target.result).then(function(buffer){
 
       let source = audioCtx.createBufferSource();
-      // source.buffer = buffer;
-      // console.log(buffer);
-      // reversing two channels buffer
       Array.prototype.reverse.call( buffer.getChannelData(0) );
       Array.prototype.reverse.call( buffer.getChannelData(1) );
       console.log(buffer.numberOfChannels);
@@ -31,7 +26,6 @@ function process() {
       audioEl = source;
     });
   };
-  
   reader1.readAsArrayBuffer(input.files[0]);
 }
 
@@ -40,20 +34,3 @@ stopBtn.addEventListener('click', ()=> {
     audioEl.stop();
     audioEl.disconnect(audioCtx.destination);
 })
-
-
-
-
-// input.addEventListener('change', handleFile, false);
-
-// function handleFile(){
-//   const files = this.files;
-  
-//   const normal = files[0];
-//   const backmasked;
-
-//   console.log(files)
-
-//   audio1.src = URL.createObjectURL(files[0]);
-//   audio1.load();
-// }
